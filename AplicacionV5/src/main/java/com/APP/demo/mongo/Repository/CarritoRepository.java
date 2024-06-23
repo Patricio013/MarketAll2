5a6633535076ad0865bd7f62aabf6cd66517b033
Patricio013 <146967055+Patricio013@users.noreply.github.com>
2024-06-23 19:49:11 -0300

    Delete AplicacionV5 directory

--- AplicacionV5/src/main/java/com/APP/demo/mongo/Repository/CarritoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.APP.demo.mongo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.APP.demo.mongo.Documentos.Carrito;
-import java.util.Optional;
-
-public interface CarritoRepository extends MongoRepository<Carrito, String>{
-	Optional<Carrito> findByUserId(String userId);
-}
--- AplicacionV5/src/main/java/com/APP/demo/mongo/Repository/ProductoLogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.APP.demo.mongo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.APP.demo.mongo.Documentos.ProductoLog;
-
-public interface ProductoLogRepository extends MongoRepository<ProductoLog, String>{
-
-}
