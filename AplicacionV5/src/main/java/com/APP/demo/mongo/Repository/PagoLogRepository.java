71a4689b779cfa3b55e4733a9efe8193de55eb3d
Patricio013 <146967055+Patricio013@users.noreply.github.com>
2024-06-26 21:25:08 -0300

    Delete AplicacionV5 directory

--- AplicacionV5/src/main/java/com/APP/demo/mongo/Repository/PagoLogRepository.java
+++
@@ -1,8 +0,0 @@
-package com.APP.demo.mongo.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.APP.demo.mongo.Documentos.PagoLog;
-
-public interface PagoLogRepository extends MongoRepository<PagoLog, String>{
-
-}
